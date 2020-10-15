dff656464821ae05cbfd2f1f7e1b14d586e98195
GallonOfPcp <70856436+GallonOfPcp@users.noreply.github.com>
2020-10-15 19:02:24 +0300

    added scroll btn
    added scroll btn

---
+++ assets/js/scrolling.js
@@ -0,0 +1,16 @@
+let scrollBtn = document.querySelector('.scroll-btn');
+
+scrollBtn.onclick = function () {
+  window.scrollTo ({
+    top: 0,
+    behavior: "smooth"
+  });
+};
+
+window.onscroll = function () {
+  if (pageYOffset > 500) {
+    scrollBtn.classList.add('visible');
+  } else {
+    scrollBtn.classList.remove('visible')
+  }
+};
